6c4adbd4b8f1309e150d191fb71e788132df98d2
Manoel Henrique <140463178+ManoelhenriqueF@users.noreply.github.com>
2025-06-10 20:44:35 -0300

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require('electron')
-const path = require('path')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getAssetPath: (...paths) => path.join(__dirname, 'assets', ...paths)
-})
